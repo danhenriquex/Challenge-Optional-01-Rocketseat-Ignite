4142a83539d29a033746d072024248bd296144f9
danilohenrique <danilosantana@mat.ci.ufpb.br>
2021-08-30 22:06:44 -0300

    Finishing optional challenge 01 ignite

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+export interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface Props {
+  genres: GenreResponseProps[];
+  handleClickButton: (id: number) => any;
+  idGenre: number;
+}
+
+export function SideBar({ genres, handleClickButton, idGenre }: Props) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={idGenre === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
